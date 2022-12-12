a0f7c38e8ece5fea0439a873ad5266544b61b46d
Elijah Mylcev <elijahmylcev@gmail.com>
2022-12-12 15:56:50 +0600

    add animation for Solution

--- src/components/Solution.vue
+++ src/components/Solution.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="solution">
-    <h2 v-if="currentTask == ''" class="solution__title">Выберите задание</h2>
-    <h2 v-else>{{ currentTask }}</h2>
+    <div v-if="currentTask == ''">
+      <h2 class="solution__title">Выберите задание</h2>
+      <div class="circle">
+        <div class="circle-u"></div>
+      </div>
+
+    </div>
+    <div v-else>
+      <h2>{{ currentTask }}</h2>
+    </div>
   </div>
 </template>
 
@@ -14,6 +22,45 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped>
+.circle {
+  margin: 15px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  border: 2px dashed rgba(30, 30, 209, .6);
+  animation: rotate 1000ms infinite;
+}
 
+.circle-u {
+  width: 85px;
+  height: 85px;
+  border-radius: 100%;
+  border: 2px dashed rgba(7, 7, 92, 0.6);
+  box-shadow: 1px 1px 15px rgba(30, 30, 209, .6);
+  animation: rotate-u 800ms infinite;
+}
+
+@keyframes rotate-u {
+  from {
+    transform: rotate(-180deg);
+  }
+
+  to {
+    transform: rotate(-360deg);
+  }
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(180deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
